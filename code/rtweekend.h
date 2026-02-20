bed83dd930c77d275c89fed76dd16836562f644e
kiran <basawarajbiradar007@gmail.com>
2026-02-20 06:51:04 +0530

    few changes for infinity

--- code/rtweekend.h
+++ code/rtweekend.h
@@ -3,7 +3,6 @@
 #include "defines.h"
 
 // Constants
-const f64 infinity = 1.7976931348623157E+308;
 const f64 pi = 3.1415926535897932385;
 // Utility Functions
 inline f64
@@ -13,6 +12,7 @@ degrees_to_radians(f64 degrees){
 
 // Common Headers
 #include "color.h"
+// #include "interval.h"
 #include "ray.h"
 #include "vec3.h"
 
